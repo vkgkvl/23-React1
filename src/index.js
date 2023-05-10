4aef8d3dc880bf53b0a2d0990ada8e0864339787
vkgkvl <vkgkvl@gmail.com>
2023-05-10 18:18:36 +0900

    .
    .

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import Comment from './chapter_05/Comment';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock/>
+    <Accommodate/>
   </React.StrictMode>
 );
 
